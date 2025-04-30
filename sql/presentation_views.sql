8c2a1313674587abe550653d61ecfde573a9c7e5
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2025-04-30 09:52:42 -0300

    Add the other presentation views for customers and products

--- sql/presentation_views.sql
+++ sql/presentation_views.sql
@@ -19,10 +19,37 @@ SELECT
     order_item_id,
     order_id,
     product_id,
-    quanity AS quantity,  -- Alias the typo'd column as 'quantity'
+    quanity AS quantity,
     updated_at,
     updated_by,
     created_at,
     created_by
 FROM intermediate.order_items
+WHERE is_current = true;
+
+CREATE OR REPLACE VIEW presentation.products AS
+SELECT
+    product_id,
+    product_name,
+    barcode,
+    unity_price,
+    is_active,
+    updated_at,
+    updated_by,
+    created_at,
+    created_by
+FROM intermediate.products
+WHERE is_current = true;
+
+CREATE OR REPLACE VIEW presentation.customers AS
+SELECT
+    customer_id,
+    customer_name,
+    is_active,
+    customer_address,
+    updated_at,
+    updated_by,
+    created_at,
+    created_by
+FROM intermediate.customers
 WHERE is_current = true;
