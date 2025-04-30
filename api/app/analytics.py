b569cdb60c56313a8753cebc83684d1ff55a167c
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2025-04-30 20:10:15 -0300

    Update the API queries

--- api/app/analytics.py
+++ api/app/analytics.py
@@ -11,7 +11,7 @@ def get_open_orders(db: Session):
     from
         presentation.orders
     where
-        status != 'COMPLETED'
+        status <> 'COMPLETED'
     group by
         delivery_date,
         status
@@ -27,7 +27,7 @@ def get_top_delivery_dates(db: Session, limit: int):
     from
         presentation.orders
     where
-        status != 'COMPLETED'
+        status <> 'COMPLETED'
     group by
         delivery_date
     order by 
@@ -41,42 +41,59 @@ def get_open_products(db: Session):
     query = """
     select
         oi.product_id,
-        sum(oi.quanity) total_items
+        sum(oi.quantity) total_items
     from
         presentation.order_items oi
     join
         presentation.orders o
         on oi.order_id = o.order_id
-    where
-        o.status != 'COMPLETED'
+    where 
+        o.status <> 'COMPLETED'
     group by
         oi.product_id
     order by
         total_items desc,
-        oi.product_id
+        oi.product_id;
     """
     return db.execute(text(query)).fetchall()
 
 
 def get_top_customers(db: Session, limit: int):
     query = f"""
-    select
-        c.customer_id,
-        c.customer_name,
-        count(1) quantity
-    from
-        presentation.orders o
-    join
-        presentation.customers c
-        on o.customer_id = c.customer_id
-    where
-        o.status != 'COMPLETED'
-    group by
-        c.customer_id,
-        c.customer_name
-    order by
-        quantity desc,
-        customer_id
-    limit {limit}
+    WITH customer_pending_orders AS (
+        SELECT
+            c.customer_id,
+            c.customer_name,
+            COUNT(*) AS quantity
+        FROM
+            presentation.orders o
+            JOIN presentation.customers c ON o.customer_id = c.customer_id
+        WHERE
+            o.status = 'PENDING'
+        GROUP BY
+            c.customer_id,
+            c.customer_name
+        ),
+        ranked_customers AS (
+        SELECT
+            customer_id,
+            customer_name,
+            quantity,
+            DENSE_RANK() OVER (ORDER BY quantity DESC) AS ranking
+        FROM
+            customer_pending_orders
+        )
+        SELECT
+        customer_id,
+        customer_name,
+        quantity,
+        ranking
+        FROM
+        ranked_customers
+        WHERE
+        ranking <= {limit}
+        ORDER BY
+        ranking,
+        customer_id;
     """
     return db.execute(text(query)).fetchall()
