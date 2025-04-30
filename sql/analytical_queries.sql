d320f690c7e2d98d3c6d87e3f4e6990587ce66d1
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2025-04-30 20:10:37 -0300

    Update analytical queries SQL file

--- sql/analytical_queries.sql
+++ sql/analytical_queries.sql
@@ -5,7 +5,7 @@ select
   status,
   count(1) quantity
 from
-	operations.orders
+	presentation.orders
 where
 	status != 'COMPLETED'
 group by
@@ -18,7 +18,7 @@ select
 	delivery_date,
   count(1) quantity
 from
-	operations.orders
+	presentation.orders
 where
 	status != 'COMPLETED'
 group by
@@ -34,12 +34,12 @@ select
   oi.product_id,
   sum(oi.quanity) total_items
 from
-	operations.order_items oi
+	presentation.order_items oi
 join
-	operations.orders o
+	presentation.orders o
 	on oi.order_id = o.order_id
 where 1=1
-	and o.status != 'COMPLETED'
+	--and o.status != 'COMPLETED'
 group by
 	oi.product_id
 order by
@@ -48,21 +48,38 @@ order by
 ;
 
 -- Top 3 Customers with more pending orders
-select
-	c.customer_id,
-  c.customer_name,
-  count(1) quantity
-from
-	operations.orders o
-join
-	operations.customers c
-	on o.customer_id = c.customer_id
-where
-	o.status != 'COMPLETED'
-group by
-	c.customer_id,
-  c.customer_name
-order by
-	quantity desc,
-  customer_id
-  ;
+WITH customer_pending_orders AS (
+  SELECT
+    c.customer_id,
+    c.customer_name,
+    COUNT(*) AS quantity
+  FROM
+    presentation.orders o
+    JOIN presentation.customers c ON o.customer_id = c.customer_id
+  WHERE
+    o.status = 'PENDING'
+  GROUP BY
+    c.customer_id,
+    c.customer_name
+),
+ranked_customers AS (
+  SELECT
+    customer_id,
+    customer_name,
+    quantity,
+    DENSE_RANK() OVER (ORDER BY quantity DESC) AS ranking
+  FROM
+    customer_pending_orders
+)
+SELECT
+  customer_id,
+  customer_name,
+  quantity,
+  ranking
+FROM
+  ranked_customers
+WHERE
+  ranking <= 3
+ORDER BY
+  ranking,
+  customer_id;
